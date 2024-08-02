8a8f68302754c332ad69b7bc56352965a0fd107b
Solbhin <leeesb923@gmail.com>
2024-08-02 16:45:41 +0900

    이력서 업로드

---
+++ albaone/src/main/java/com/springmvc/repository/ResumeRespository.java
@@ -0,0 +1,12 @@
+package com.springmvc.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.springmvc.domain.Resume;
+
+@Repository
+public interface ResumeRespository {
+
+	void setmyImg(Resume resume);
+
+}
---
+++ albaone/src/main/java/com/springmvc/service/ResumeService.java
@@ -0,0 +1,7 @@
+package com.springmvc.service;
+
+import com.springmvc.domain.Resume;
+
+public interface ResumeService {
+	void setmyImg(Resume resume);
+}
